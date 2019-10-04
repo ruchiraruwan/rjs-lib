3627403a58d3a4a5aa46dd0a4811d20e9e44e71e
Ruchira <uarruwan@gmail.com>
2019-10-04 23:20:06 +0530

    remove perviously searched highlighted text

--- js/scripts.js
+++ js/scripts.js
@@ -11,34 +11,44 @@
  *              All the functionalities can be integrated easily.
  * License: 
  */
- 
+
+ // RJS : Higlight search word in the current page 
+ var contentTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'span', 'label'];
  var foundCount = 0;
+ var previousSearchText = "";
  
- // RJS : Higlight search word in the current page
  var handleSearchBtnAction = function(){
 	var searchText = document.getElementById("rjs-search-text").value;
     foundCount = 0;
 	
 	// reset the resut section
 	document.getElementById("rjs-search-result").innerHTML = '';
-	document.body.classList.remove("rjs-highlight");
 	if (searchText.length > 0) {
-	    var contentTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'span', 'label'];
-		for(var i = 0; i < contentTags.length ; i++) {			
-			// Will return a NodeList even if there is only one element found
-			var contents = document.querySelectorAll(contentTags[i]);
-			if (contents.length > 0) {
-				for (var j=0; j < contents.length; j++) {
-					contents[j].innerHTML = contents[j].innerHTML.replace(new RegExp(searchText, 'g'),function(){ foundCount += 1 ; return "<span class='rjs-highlight'>"+searchText+"</span>";});
-				}
-			}
-		}	
+		if (previousSearchText === "") {
+			previousSearchText = searchText;
+		} else {
+			// clear previous highlighted texts
+			replaceContent('<span class="rjs-highlight">'+previousSearchText+'</span>', previousSearchText);
+			previousSearchText = searchText;
+		}
+		replaceContent(new RegExp(searchText, 'g'), function(){ foundCount += 1 ; return "<span class=\"rjs-highlight\">"+searchText+"</span>";});
 		document.getElementById("rjs-search-result").innerHTML = "(Found: "+foundCount/2+")";
 	} else {
 		document.getElementById("rjs-search-result").innerHTML = "<span class='rjs-error'>Search text cannot be empty</span>";
 	}
-	return;
  };
+ 
+ var replaceContent = function(regexPattern, replaceFn) {
+	for(var i = 0; i < contentTags.length ; i++) {			
+		// Will return a NodeList even if there is only one element found
+		var contents = document.querySelectorAll(contentTags[i]);
+		if (contents.length > 0) {
+			for (var j=0; j < contents.length; j++) {
+				contents[j].innerHTML = contents[j].innerText.replace(regexPattern, replaceFn);
+			}
+		}
+	}
+ }
  
  document.getElementById("rjs-search-text").addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
